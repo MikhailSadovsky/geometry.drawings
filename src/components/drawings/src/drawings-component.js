7d14c5af65aa711b3cd7bd98c9fb0ed776687e1d
alexei-yasko <alexei.yasko@gmail.com>
2014-10-18 19:59:16 +0300

    component refactoring

--- src/components/drawings/src/drawings-component.js
+++ src/components/drawings/src/drawings-component.js
@@ -2,27 +2,26 @@
  * Drawings component.
  */
  
- GeomDrawComponent = {
+Drawings.GeomDrawComponent = {
     ext_lang: 'geometry_code',
     formats: ['format_geometry_json'],
+
     factory: function(sandbox) {
-        return new geomDrawWindow(sandbox);
+        return new Drawings.GeomDrawWindow(sandbox);
     }
 };
 
-var geomDrawWindow = function(sandbox) {
-
-    this.domContainer = sandbox.container;
+Drawings.GeomDrawWindow = function(sandbox) {
     this.sandbox = sandbox;
-    
+
     this.model = new Drawings.Model();
-    this.paintPanel = new Drawings.PaintPanel(this.domContainer, this.model);
+    this.paintPanel = new Drawings.PaintPanel(this.sandbox.container, this.model);
     this.paintPanel.init();
 
     this.recieveData = function(data) {
         /// @todo Process data - json file data
     };
-    
+
     this.eventStructUpdate = function(added, element, arc) {
         window.sctpClient.get_arc(arc).done(function (r) {
             /// @todo Process r.result[1] - element in contour
@@ -36,7 +35,4 @@ var geomDrawWindow = function(sandbox) {
     this.sandbox.updateContent();
 };
 
-
-
-SCWeb.core.ComponentManager.appendComponentInitialize(GeomDrawComponent);
-
+SCWeb.core.ComponentManager.appendComponentInitialize(Drawings.GeomDrawComponent);

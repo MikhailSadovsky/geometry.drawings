ed5f6283ad7d54c23f4baa360626e9cbb5d24c40
Andrey <a.bakhmetow@yandex.ru>
2014-10-18 18:56:41 +0300

    Mouse events
    Click

--- src/components/drawings/src/editor/drawings-controller.js
+++ src/components/drawings/src/editor/drawings-controller.js
@@ -38,25 +38,28 @@ Drawings.Controller.prototype = {
     },
 
     handleMouseMoveEvent: function(event) {
-//        console.log('mousemove, paintPanelMouseMoved = ' + this.paintPanel.mouseMoved);
         this.paintPanel.mouseMoved = true;
     },
 
     _handleLeftMouseDownEvent: function(event) {
-//        console.log("controller_mouseDownEvent, paintPanel.mouseMoved=" + paintPanel.mouseMoved);
+        this.paintPanel.mouseDownEvent = event;
     },
 
     _handleLeftMouseUpEvent: function(event) {
-//        console.log("controller_mouseUpEvent, paintPanel.mouseMoved=" + paintPanel.mouseMoved);
-        if (this.paintPanel.mouseMoved == false) {
-            this._handleLeftMouseClickEvent(event);
-        }
+        this._validateClick(this.paintPanel.mouseDownEvent, event);
     },
 
     _handleLeftMouseClickEvent: function(event) {
-//        console.log("controller_mouseClickEvent, paintPanel.mouseMoved=" + paintPanel.mouseMoved);
         var point = this._getPoint(event);
         this._addPoint(point);
+    },
+
+    _validateClick: function(mouseDownEvent, mouseUpEvent) {
+        var mouseDownCoordinates = this.paintPanel.getMouseCoordinates(mouseDownEvent);
+        var mouseUpCoordinates = this.paintPanel.getMouseCoordinates(mouseUpEvent);
+        if (Math.sqrt(Math.pow(mouseDownCoordinates[0] - mouseUpCoordinates[0], 2) + Math.pow(mouseDownCoordinates[1] - mouseUpCoordinates[1], 2)) < 0.25) {
+            this._handleLeftMouseClickEvent(mouseUpEvent);
+        }
     },
 
     _getPoint: function(event) {

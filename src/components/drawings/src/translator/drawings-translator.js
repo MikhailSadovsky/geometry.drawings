e0dfccb7a5c4a5b858718715b8b243f5912f6a8e
alexei-yasko <alexei.yasko@gmail.com>
2014-10-19 23:00:50 +0300

    parse model from json (intermediate commit)

--- src/components/drawings/src/translator/drawings-translator.js
+++ src/components/drawings/src/translator/drawings-translator.js
@@ -8,6 +8,15 @@ Drawings.Translator = {
         return JSON.stringify(this._prepareModelForSerialization(model));
     },
 
+    fromJson: function (json) {
+        var jsonModel = JSON.parse(json);
+
+        var points = this._fromJsonPoints(jsonModel.points);
+        var shapes = this._fromJsonShapes(jsonModel.shapes, points);
+
+        return [points, shapes];
+    },
+
     _prepareModelForSerialization: function (model) {
         var points = model.getPoints();
         var shapes = [];
@@ -16,6 +25,26 @@ Drawings.Translator = {
             shapes.push({className: shape.constructor.name, name: shape.getName(), points: shape.getPoints()})
         });
 
-        return {model: {points: points, shapes: shapes}};
+        return {points: points, shapes: shapes};
+    },
+
+    _fromJsonPoints: function(jsonPoints) {
+        var points = [];
+
+        for (var i = 0; i < jsonPoints.length; i++) {
+            var jsonPoint = jsonPoints[i];
+
+            var point = new Drawings.Point(jsonPoint.x, jsonPoint.y);
+            point.setName(jsonPoint.name);
+
+            points.push(point);
+        }
+
+        return points;
+    },
+
+    _fromJsonShapes: function(jsonShapes, points) {
+        var shapes = [];
+        return shapes;
     }
 };

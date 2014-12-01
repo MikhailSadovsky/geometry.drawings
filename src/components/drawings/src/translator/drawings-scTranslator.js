ed569863fc5b7093795c05e31e891a9b5bb28b41
Aliaksei Parkalau <a.parkalov@gmail.com>
2014-12-01 12:06:26 +0300

    [translator] points, resolving system nodes added

--- src/components/drawings/src/translator/drawings-scTranslator.js
+++ src/components/drawings/src/translator/drawings-scTranslator.js
@@ -15,7 +15,7 @@ Drawings.ScTranslator = {
 			window.sctpClient.find_element_by_system_identifier(node_name).done(function(r){
 			
 				if(r.resCode != SctpResultCode.SCTP_RESULT_OK){
-					alert("can't resolve " + node_name);
+					alert("can't resolve " + node_name);//TODO: remove alert
 					//throw "Can't resolve node";
 				}	
 
@@ -40,11 +40,24 @@ Drawings.ScTranslator = {
 		var my_array = [];
 		var self = this;
 		
-		my_array.push(this.getKeyNode("big_red_node")); //TODO: add more
-		my_array.push(this.getKeyNode("concept_triangle")); //TODO: add more
+		my_array.push(this.getKeyNode("concept_segment"));
+		my_array.push(this.getKeyNode("nrel_side"));
+		my_array.push(this.getKeyNode("concept_triangle"));
+		my_array.push(this.getKeyNode("concept_geometric_point"));//?
+		my_array.push(this.getKeyNode("concept_line"));
+		my_array.push(this.getKeyNode("nrel_boundary_point"));
+		my_array.push(this.getKeyNode("nrel_inclusion"));
+		my_array.push(this.getKeyNode("nrel_vertex"));
+		my_array.push(this.getKeyNode("nrel_length"));
+		my_array.push(this.getKeyNode("concept_value"));
+		//my_array.push(this.getKeyNode("concept_value_2")); //TODO: add value in
+		//my_array.push(this.getKeyNode("XXXXXXXX")); //TODO: add sm
+		my_array.push(this.getKeyNode("nrel_area"));
+		my_array.push(this.getKeyNode("concept_square"));
+		my_array.push(this.getKeyNode("big_red_node")); // 14
+		
 		$.when.apply($, my_array).done(function (){
-		alert("resolved 1: "+ self.big_red_node);
-		alert("resolved 2: "+ self.concept_triangle);
+		//alert("resolved 1: "+ self.big_red_node);//TODO: remove alert
 			dfd.resolve();
 		
 		}).fail(function () {
@@ -54,18 +67,72 @@ Drawings.ScTranslator = {
 		return dfd.promise();
 	},
 
+//~~~~~~~~~~~
+    putPoint: function (point) {
+
+		var dfd = new jQuery.Deferred();
+			
+		if (point.hasOwnProperty("sc_addr") && point.sc_addr != null){
+			dfd.resolve(point.sc_addr);
+		//	alert("point resolved: " + point.sc_addr);//TODO: remove alert
+			return dfd.promise();
+		}
+			
+		var self = this;
+		point.sc_addr = null;
+
+		window.sctpClient.create_node(sc_type_node | sc_type_const).done(function (r){
+			//point.setScAddr(r.result);
+			point.sc_addr = r.result;
+
+			window.sctpClient.create_arc(sc_type_arc_pos_const_perm, r.result, self.big_red_node).done(function (){
+				dfd.resolve(r.result);
+			}).fail(function(){
+				point.sc_addr = null;
+				dfd.reject();
+				alert("create arc for point failed");
+			});
+		}).fail(function(){
+			point.sc_addr = null;
+			dfd.reject();
+			alert("create node for point failed");
+		});
+			//dfd.resolve(r.result);
+		
+    return dfd.promise();
+    },
+
+	pushPoints: function(points){
+	
+		var dfd = new jQuery.Deferred();
+		var my_array = [];
+		var self = this;
+		
+		for (t in points){
+			my_array.push(this.putPoint(points[t]));
+		};
+
+		$.when.apply($, my_array).done(function (){
+		///alert("resolved pushPoints: "+ points.length);//TODO: remove alert
+			dfd.resolve();
+		
+		}).fail(function () {
+			dfd.reject();
+		});
+		
+		return dfd.promise();
+	},
     // temporary solution for keeping KB clean
     wipeOld: function() {
-        //find 'big_red_node' that contains all new data and wipe out contents
-
-	alert("resolved 3: " + this.big_red_node);
-	console.log(this);
-/*		
-	var dfd = new jQuery.Deferred();
-    
-	this.sctp_client.iterate_elements(SctpIteratorType.SCTP_ITERATOR_3F_A_A,
+        
+		//find 'big_red_node' that contains all new data and wipe out contents - deleting won't work yet =\
+		
+		var self = this;
+		var dfd = new jQuery.Deferred();
+/*	
+		window.sctpClient.iterate_elements(SctpIteratorType.SCTP_ITERATOR_3F_A_A,
                                       [
-                                          this.bigred,
+                                          self.big_red_node,
                                           sc_type_arc_pos_const_perm,
                                           sc_type_node | sc_type_const
                                       ])
@@ -75,35 +142,17 @@ Drawings.ScTranslator = {
         	for (r in res.result) {
         	    langs.push(res.result[r][2]);
         	}
-        
-        	dfd.resolve(langs);
-        
+        //TODO: add deleting when it will work
+		
+		dfd.resolve(langs);
+       
     	}).fail(function () {
-        	dfd.reject();
+			alert("fail in wipeOld");
+        	dfd.resolve();
     	});
-
-	return dfd.promise();
-*/	    
-    },
-
-    putPoint: function (point) {
-	//	if (point.hasOwnProperty("sc_addr") && point.sc_addr != 'null')
-	//TODO: add existence check if needed	
-	if (point.sc_addr) 
-	{
-		return point.sc_addr;
-	}
-        //else
-	//{
-	//	window.sctpClient.create_node(scg-type-node-const).done(function (r) 
-	//	{
-        //            point.setScAddr(r.result);
-        //        //    point.setObjectState(SCgObjectState.NewInMemory);//not used yet
-        //            
-        //        });
-	//	return point.sc_addr;
-        //}
-        return "sc_addr";
+*/
+		dfd.resolve();
+		return dfd.promise();
     },
 
     putShape: function (shape) {
@@ -112,22 +161,30 @@ Drawings.ScTranslator = {
 
     putModel: function (model) {
 	//cleanup
-	var func = this.wipeOld;
+	var cleanup = this.wipeOld;
+	//adding points
+	var pushPts = this.pushPoints;
 	var self = this;
-	this.getKeyNodes().done(function () {	func.call(self);})
 
+	var dfd = this.getKeyNodes().done(function () {
+		//cleanup.call(self);
+		console.log(self);//TODO: remove logging resolved system nodes
 
-	//foreach point create sc-node and add sc_addr
-	//foreach shape add shape-defined nodes and arcs
-	alert("started");
-	for (var i = 0; i < model.points.length; i++) 
-	{
-		var el = model.points[i];
-        	this.putPoint(el);
-	}
-	
-	alert("completed");
+		//foreach point add point-defined nodes and arcs
+		pushPts.call(self, model.points);
+	});
+
+	$.when(dfd).done(function (){
+		//foreach shape add shape-defined nodes and arcs
+
+		for (var i = 0; i < model.points.length; i++) 
+		{
+			var el = model.points[i];
+		}
+		alert(model.points.length + " points translated; ");
         
+	});		
+	
     }    
 
 };

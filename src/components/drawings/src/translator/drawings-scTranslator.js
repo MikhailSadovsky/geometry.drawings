983ccb3f3ad5f4ef66a41e9ae2970ff1344351b0
MikhailSadovsky <mishachess@mail.ru>
2015-05-04 13:40:37 +0300

    add length translating

--- src/components/drawings/src/translator/drawings-scTranslator.js
+++ src/components/drawings/src/translator/drawings-scTranslator.js
@@ -56,6 +56,28 @@ Drawings.ScTranslator = {
     },
 
     /*
+     Add link with content into base_el.
+     All parameters must be sc_addr
+     */
+    addNewLinkWithContent: function (content, base_el) {
+        window.sctpClient.create_link().done(function (res) {
+            window.sctpClient.create_arc(sc_type_arc_pos_const_perm, self.big_red_node, res);
+            window.sctpClient.set_link_content(res, content);
+            self.addFiveConstructionIntoBase(r, res, self.nrel_system_identifier, base_el,
+                sc_type_arc_common | sc_type_const);
+        });
+    },
+
+    /*
+     Add relation and quantity-value-answer construction.
+     See segment length example
+     relation must be sc_addr, value is an answer (for example shape.length)
+     */
+    addConstructionWithValueAndQuantity: function (relation, value) {
+
+    },
+
+    /*
      Add relation or attribute construction.
      All parameters must be sc_addr
      */
@@ -148,7 +170,37 @@ Drawings.ScTranslator = {
                         self.addFiveConstructionIntoBase(r, points[i].sc_addr, self.nrel_boundary_point,
                             self.big_red_node, sc_type_arc_common | sc_type_const);
                     }
-
+                    if (shape.length) {
+                        self.addConstructionWithValueAndQuantity(self.nrel_length, shape.length);
+                        var arc1 = window.sctpClient.create_arc(sc_type_arc_pos_const_perm, self.big_red_node, self.nrel_length);
+                        arc1.done(function (r1) {
+                            var arc2 = window.sctpClient.create_arc(sc_type_arc_pos_const_perm, self.big_red_node, self.nrel_value);
+                            arc2.done(function (r2) {
+                                window.sctpClient.create_arc(sc_type_arc_pos_const_perm, self.big_red_node, self.concept_quantity);
+                                window.sctpClient.create_node(sc_type_node | sc_type_const).done(function (quality_node) {
+                                    window.sctpClient.create_arc(sc_type_arc_pos_const_perm, self.big_red_node, quality_node);
+                                    window.sctpClient.create_node(sc_type_node | sc_type_const).done(function (value_node) {
+                                        window.sctpClient.create_arc(sc_type_arc_pos_const_perm, self.big_red_node, value_node);
+                                        window.sctpClient.create_node(sc_type_node | sc_type_const).done(function (answer_node) {
+                                            self.addFiveConstruction(self.concept_quantity, quality_node, self.big_red_node, sc_type_arc_pos_const_perm);
+                                            window.sctpClient.create_arc(sc_type_arc_pos_const_perm, self.big_red_node, answer_node);
+                                            self.addFiveConstructionIntoBase(r, quality_node, self.nrel_length,
+                                                self.big_red_node, sc_type_arc_common | sc_type_const);
+                                            self.addFiveConstructionIntoBase(value_node, quality_node, self.nrel_value,
+                                                self.big_red_node, sc_type_arc_common | sc_type_const);
+                                            self.addFiveConstruction(value_node, answer_node, self.big_red_node, sc_type_arc_pos_const_perm);
+                                            window.sctpClient.create_link().done(function (res) {
+                                                window.sctpClient.create_arc(sc_type_arc_pos_const_perm, self.big_red_node, res);
+                                                window.sctpClient.set_link_content(res, shape.length);
+                                                self.addFiveConstructionIntoBase(answer_node, res, self.nrel_system_identifier, self.big_red_node,
+                                                    sc_type_arc_common | sc_type_const);
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    }
                 }
                 if (shape.className == 'Line') {
                     shapeType = self.concept_straight_line;

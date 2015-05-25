cc971b4dc9ad56093ac9a4a117165a878a5eb8b4
MikhailSadovsky <mishachess@mail.ru>
2015-05-25 18:02:50 +0300

    Search definition by agent

--- src/components/drawings/src/editor/controller/drawings-circleController.js
+++ src/components/drawings/src/editor/controller/drawings-circleController.js
@@ -32,10 +32,38 @@ Drawings.CircleController.prototype = {
         var setDefinitionMenuItem = {
             text: 'Просмотр определения',
             action: function(){
-                window.sctpClient.get_link_content(565510145,'string').done(function(content)
-                {
-
-                    $('#textArea').val(content);
+                var addr;
+                var attr;
+                SCWeb.core.Server.resolveScAddr(['rrel_finding_definition'], function (keynodes) {
+                    attr = keynodes['rrel_finding_definition'];
+                });
+                SCWeb.core.Server.resolveScAddr(['concept_circle'], function (keynodes) {
+                    addr = keynodes['concept_circle'];
+                    SCWeb.core.Server.resolveScAddr(["ui_menu_file_for_finding_definitions"],
+                        function (data) {
+                            var cmd = data["ui_menu_file_for_finding_definitions"];
+                            SCWeb.core.Server.doCommand(cmd,
+                                [addr], function (result) {
+                                    if (result.question != undefined) {
+                                        var date = new Date();
+                                        var curDate = null;
+                                        do {curDate = new Date();}
+                                        while (curDate-date < 3000);
+                                        window.sctpClient.iterate_elements(SctpIteratorType.SCTP_ITERATOR_5F_A_A_A_F, [
+                                            addr,
+                                            sc_type_arc_pos_const_perm,
+                                            sc_type_link,
+                                            sc_type_arc_pos_const_perm,
+                                            attr]).
+                                            done(function(linkNode){
+                                                window.sctpClient.get_link_content(linkNode[0][2],'string').done(function(content)
+                                                {
+                                                    $('#textArea').val(content);
+                                                });
+                                            });
+                                    }
+                                });
+                        });
                 });
             }
         }
